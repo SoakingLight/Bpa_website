5978336d650831c27cdc08356c44340b299f8778
cereemo <sebashnagara@gmail.com>
2022-12-01 09:30:50 -0500

    op op op oppum gangam style

--- BPA website/divcreator.js
+++ BPA website/divcreator.js
@@ -1,6 +1,17 @@
-let data = '[ {"brand": "Ford","model": "maverick","year": 2022,"color": "black","price": "10,000","imageURL": "assets/testimage.png"}, {"brand": "Bugatti","model": "the tate","year": 2019,"color": "red","price": "1,000,000","imageURL": ""}]';
-
 const vehicleArray = JSON.parse(data);
+let finalDivOutput = "";
 
-document.getElementById("main").innerHTML = `<div class = "vehicleDiv"> <img height = "200px" src = "${vehicleArray[0].imageURL}"> <h1> ${vehicleArray[0].year + " " + vehicleArray[0].color + " " + vehicleArray[0].brand + " " + vehicleArray[0].model} </h1> </div>`;
+for (let i = 0; i != vehicleArray.length; i++) {
+finalDivOutput += 
+`<div class = "vehicleDiv" ${(i%2 == 0)? "" : 'style = "float: right;"'}> 
+<img height = "200px" src = "${vehicleArray[i].imageURL}"> 
+<h1> ${vehicleArray[i].year + " " + vehicleArray[i].color + " " + vehicleArray[i].brand + " " + vehicleArray[i].model} </h1> 
+<h2 style = "margin-left: 450px;"> Price: ${"₿" + vehicleArray[i].price} </h2>
+<p style = "margin-left: 450px; margin-right: 40px;"> ${vehicleArray[i].description} <br> <br>
+    <button> BUY NOW! </button>    
+</p>
+</div>`
+}
+
+document.getElementById("main").innerHTML = finalDivOutput;
 
