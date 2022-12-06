79198d4ec3342b4e912de1520d6bf625114382f6
cereemo <sebashnagara@gmail.com>
2022-12-05 21:44:57 -0500

    peepee poopoo

--- BPA website/divcreator.js
+++ BPA website/divcreator.js
@@ -1,25 +1,37 @@
 const vehicleArray = JSON.parse(data);
+function pageWriter (argumentArray) {
 let finalDivOutput = "";
+var global;
 
-for (let i = 0; i != vehicleArray.length; i++) {
+for (let i = 0; i != argumentArray.length; i++) {
 finalDivOutput += 
 `<div class = "vehicleDiv" ${(i%2 == 0)? 'style = "margin-right: 0;"' : ""}> 
-<img height = "200px" src = "${vehicleArray[i].imageURL}"> 
-<h1> ${vehicleArray[i].year + " " + vehicleArray[i].color + " " + vehicleArray[i].brand + " " + vehicleArray[i].model} </h1> 
-<h2 style = "margin-left: 450px;"> Price: ${"₿" + vehicleArray[i].price} </h2>
-<p style = "margin-left: 450px; margin-right: 40px;"> ${vehicleArray[i].description} <br> <br>
+<img height = "200px" src = "${argumentArray[i].imageURL}"> 
+<h1> ${argumentArray[i].year + " " + argumentArray[i].color + " " + argumentArray[i].brand + " " + argumentArray[i].model} </h1> 
+<h2 style = "margin-left: 450px;"> Price: ${"$" + argumentArray[i].price} </h2>
+<p style = "margin-left: 450px; margin-right: 40px;"> ${argumentArray[i].description} <br> <br>
     <button> BUY NOW! </button>    
 </p>
 </div>`
 }
-
+console.log(global);
 document.getElementById("main").innerHTML = finalDivOutput;
+}
 
-function searchHandler () {
+function searchRedirect (input) {
+    window.location.href = "vehicle.html";
+    global = input;
+}
+
+function searchInputGet () {
     const input = document.getElementById("searchbar").value;
+    searchHandler(input);
+}
+
+function searchHandler (input) {
     const inputArray = input.split(" ");
     let gate;
-    let output = "";
+    let output = [];
     for (let i = 0; i != vehicleArray.length; i++) {
         gate = false;
         for (let foo = 0; foo != inputArray.length; foo++)
@@ -30,21 +42,10 @@ function searchHandler () {
                 gate = true;
             }
         if (gate == true) {
-            output += 
-            `<div class = "vehicleDiv" ${(i%2 == 0)? 'style = "margin-right: 0;"' : ""}> 
-            <img height = "200px" src = "${vehicleArray[i].imageURL}"> 
-            <h1> ${vehicleArray[i].year + " " + vehicleArray[i].color + " " + vehicleArray[i].brand + " " + vehicleArray[i].model} </h1> 
-            <h2 style = "margin-left: 450px;"> Price: ${"₿" + vehicleArray[i].price} </h2>
-            <p style = "margin-left: 450px; margin-right: 40px;"> ${vehicleArray[i].description} <br> <br>
-                <button> BUY NOW! </button>    
-            </p>
-            </div>`
+            output[output.length] = vehicleArray[i];
         }
-    }
 
-    document.getElementById("main").innerHTML = output;
-    console.log(inputArray[0]);
-    console.log(finalDivOutput);
-    
+    }
+    pageWriter(output);
 }
 
